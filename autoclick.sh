af61c314b58a404317c947bbd20a694698686a67
Aaron Darwish <aaron7darwish@gmail.com>
2022-05-02 10:58:46 +0100

    Just added a comment to inform the user about the command that fetches the mouse position.

--- autoclick.sh
+++ autoclick.sh
@@ -1,17 +1,18 @@
+# To find a specific location, use: "xdotool getmouselocation"
+
 let stop=0
 while [ $stop -eq 0 ]; do
-    sleep 1 
 #    xdotool mousemove 1857 492 click 1 &
 #    sleep 1
 #    xdotool mousemove 1703 249 click 1 &
 #    sleep 1 
 #    xdotool mousemove 1820 335 click 1 &
-    sleep 1
-    xdotool mousemove 2028 282 click 1 &
-    sleep 3 
-    xdotool mousemove 1090 998 click 1 &
+#    sleep 1
+#    xdotool mousemove 2028 282 click 1 &
+#    sleep 3 
+    xdotool mousemove 875 959 click 1 &
     
-    sleep 2
+    sleep 0.1
     testIt=$(xset q | rg 'Caps' | sed -n 's/.*\(Caps Lock:   on\).*/\1/p')
 
     if [ "$testIt" = "" ]; then
